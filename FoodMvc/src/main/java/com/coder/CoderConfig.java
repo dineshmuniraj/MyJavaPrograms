d16ba9870a1a2e5788b17bc3515adbd1acfd48b7
dineshmuniraj <67988746+dineshmuniraj@users.noreply.github.com>
2021-02-04 20:04:24 -0800

    Add files via upload

---
+++ FoodMvc/src/main/java/com/coder/CoderConfig.java
@@ -0,0 +1,21 @@
+package com.coder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.coder"})
+public class CoderConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver()
+	{
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+
+}
